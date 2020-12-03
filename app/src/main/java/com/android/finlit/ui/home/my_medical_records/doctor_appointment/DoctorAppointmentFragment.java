7366fc4ddf10508511a4cf4ff730dfcde7f8661c
Diljot Singh <diljots99@gmail.com>
2020-12-03 19:14:09 +0530

    autocomplete text fields

--- app/src/main/java/com/android/finlit/ui/home/my_medical_records/doctor_appointment/DoctorAppointmentFragment.java
+++ app/src/main/java/com/android/finlit/ui/home/my_medical_records/doctor_appointment/DoctorAppointmentFragment.java
@@ -7,43 +7,22 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 import com.android.finlit.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DoctorAppointmentFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class DoctorAppointmentFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    AutoCompleteTextView actv_appointment_type,actv_appointment_state;
 
     public DoctorAppointmentFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DoctorAppointmentFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static DoctorAppointmentFragment newInstance(String param1, String param2) {
         DoctorAppointmentFragment fragment = new DoctorAppointmentFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -52,8 +31,7 @@ public class DoctorAppointmentFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
@@ -61,6 +39,20 @@ public class DoctorAppointmentFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_doctor_appointment, container, false);
+        View root =  inflater.inflate(R.layout.fragment_doctor_appointment, container, false);
+        actv_appointment_type = root.findViewById(R.id.actv_appointment_type);
+        actv_appointment_state = root.findViewById(R.id.actv_appointment_state);
+
+        String[] appointmentType  = new String[] {"Day Surgery", "Emergency", "Inpatient", "Outpatient" ,"Recurring"};
+        String[] appointmentState  = new String[] {"Scheduled", "To Be scheduled", "Completed", "Cancelled" ,"Missed"};
+
+        ArrayAdapter<String> appointmentTypeAdapter = new ArrayAdapter<>(getContext(),  R.layout.dropdown_menu_popup_item, appointmentType);
+        ArrayAdapter<String> appointmentStateAdapter = new ArrayAdapter<>(getContext(),  R.layout.dropdown_menu_popup_item, appointmentType);
+
+        actv_appointment_type.setAdapter(appointmentTypeAdapter);
+        actv_appointment_state.setAdapter(appointmentStateAdapter);
+
+
+        return root;
     }
 }
