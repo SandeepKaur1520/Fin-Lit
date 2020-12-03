552c4a4ef862343f868089030549c20797e53ee4
Sandeep <deepdharamsot1520@gmail.com>
2020-12-03 14:33:33 +0530

    measurement pages added

--- app/src/main/java/com/android/finlit/ui/home/my_medical_records/measurement/MeasurementFragment.java
+++ app/src/main/java/com/android/finlit/ui/home/my_medical_records/measurement/MeasurementFragment.java
@@ -3,47 +3,27 @@ package com.android.finlit.ui.home.my_medical_records.measurement;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.android.finlit.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MeasurementFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MeasurementFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    Button btn_blood_pressure,btn_pulse,btn_sugar_level,btn_height,btn_weight;
 
     public MeasurementFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MeasurementFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static MeasurementFragment newInstance(String param1, String param2) {
         MeasurementFragment fragment = new MeasurementFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -52,15 +32,83 @@ public class MeasurementFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_measurement, container, false);
+        View root =  inflater.inflate(R.layout.fragment_measurement, container, false);
+
+        btn_blood_pressure = root.findViewById(R.id.btn_blood_pressure);
+        btn_pulse = root.findViewById(R.id.btn_pulse);
+        btn_sugar_level = root.findViewById(R.id.btn_sugar_level);
+        btn_height = root.findViewById(R.id.btn_height);
+        btn_weight = root.findViewById(R.id.btn_weight);
+
+
+        btn_blood_pressure();
+        btn_pulse();
+        btn_sugar_level();
+        btn_height();
+        btn_weight();
+
+
+
+        return root;
     }
+
+    private void btn_weight() {
+        btn_weight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.action_navigation_measurement_to_navigation_weight);
+            }
+        });
+
+    }
+
+
+    private void btn_blood_pressure() {
+        btn_blood_pressure.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.action_navigation_measurement_to_navigation_blood_pressure);
+            }
+        });
+
+    }
+
+
+    private void btn_pulse() {
+        btn_pulse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.action_navigation_measurement_to_navigation_pulse);
+            }
+        });
+    }
+
+
+    private void btn_sugar_level() {
+        btn_sugar_level.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.action_navigation_measurement_to_navigation_sugar_level);
+            }
+        });
+    }
+
+
+    private void btn_height() {
+        btn_height.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.action_navigation_measurement_to_navigation_height);
+            }
+        });
+    }
+
+
 }
